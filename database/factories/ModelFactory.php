b9d45e2e3b4faffd2ed989f8f811d326911022a4
Victor <victor.ananias8@gmail.com>
2018-08-28 20:27:51 -0300

    001 initial setup - DONE

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Thread::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'user_id' => function() {
+            return factory(App\User::class)->create()->id;
+        },
+    ];
+});
+
+$factory->define(App\Reply::class, function (Faker $faker) {
+    return [
+        'body' => $faker->paragraph,
+        'thread_id' => function() {
+            return factory(App\Thread::class)->create()->id;
+        },
+        'user_id' => function() {
+            return factory(App\User::class)->create()->id;
+        },
+    ];
+});
+
+// $threads = factory(App\Thread::class, 50)->create();
+// $threads->each(function($thread) { factory(App\Reply::class, 10)->create([ 'thread_id' => $thread->id]); });
