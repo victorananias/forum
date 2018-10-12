91a5d74a0c0fe9b1d635485fec0988f7addf0c41
Victor <victor.ananias8@gmail.com>
2018-10-12 17:24:22 -0300

    forum 19 - DONE

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,6 +1,18 @@
 <div class="card mb-2"> 
     <div class="card-header">
-        <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
+    <div class="level">
+        <h5 class="flex">
+            <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
+        </h5>
+        <div>
+            <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                {{ csrf_field() }}
+                <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                    {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                </button>
+            </form>
+        </div>
+    </div>
     </div>
     <div class="card-body">
         {{ $reply->body }}
