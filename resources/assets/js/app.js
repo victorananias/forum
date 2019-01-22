0cd5bd53c725340b9930277da76ef4674d658786
victor <victor.ananias8@gmail.com>
2019-01-22 21:46:14 -0200

    32 - save

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -22,6 +22,7 @@ window.flash = function(message) {
  */
 
 Vue.component('flash', require('./components/Flash.vue'));
+Vue.component('reply', require('./components/Reply.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,29 +1,32 @@
-<div class="card mb-4" id="reply-{{ $reply->id }}"> 
-    <div class="card-header">
-    <div class="level">
-        <div class="flex">
-            <a href="/profiles/{{ $reply->owner->name }}">{{ $reply->owner->name }}</a> disse {{ $reply->created_at->diffForHumans() }}...
+<reply>
+    <div class="card mb-4" id="reply-{{ $reply->id }}"> 
+        <div class="card-header">
+        <div class="level">
+            <div class="flex">
+                <a href="/profiles/{{ $reply->owner->name }}">{{ $reply->owner->name }}</a> disse {{ $reply->created_at->diffForHumans() }}...
+            </div>
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    {{ csrf_field() }}
+                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites_count }} {{ str_plural('Favorito', $reply->favorites_count) }}
+                    </button>
+                </form>
+            </div>
         </div>
-        <div>
-            <form method="POST" action="/replies/{{ $reply->id }}/favorites">
-                {{ csrf_field() }}
-                <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                    {{ $reply->favorites_count }} {{ str_plural('Favorito', $reply->favorites_count) }}
-                </button>
-            </form>
         </div>
-    </div>
-    </div>
-    <div class="card-body">
-        {{ $reply->body }}
-    </div>
-    @can('update', $reply)
-        <div class="card-footer">
-            <form method="POST" action="/replies/{{ $reply->id }}">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-            </form>
+        <div class="card-body">
+            {{ $reply->body }}
         </div>
-    @endcan 
-</div>
+        @can('update', $reply)
+            <div class="card-footer level">
+                <button class="btn btn-secondary btn-sm mr-2">Edit</button>
+                <form method="POST" action="/replies/{{ $reply->id }}">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                </form>
+            </div>
+        @endcan 
+    </div>
+</reply>
