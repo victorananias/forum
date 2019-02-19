683e112174679306b6b8ca64933bed14e07c7651
Victor <victor.ananias8@gmail.com>
2019-02-19 15:58:00 -0300

    notifications are made through event class

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,9 +13,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\Event',
-        ],
+        'App\Events\ThreadHasNewReply' => [
+            'App\Listeners\NotifyThreadSubscribers',
+        ]
     ];
 
     /**
