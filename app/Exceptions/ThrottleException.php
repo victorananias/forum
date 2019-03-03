c8d42bf94b07c108fe3ae7a9a805f36f336938fc
victor <victor.ananias8@gmail.com>
2019-03-02 23:34:26 -0300

    MentionUsersTest created

--- app/Exceptions/ThrottleException.php
+++ app/Exceptions/ThrottleException.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: victor
- * Date: 01/03/19
- * Time: 21:36
- */
 
 namespace App\Exceptions;
-
 
 class ThrottleException extends \Exception
 {
--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreatePostRequest;
+use App\Notifications\YouWereMentioned;
 use Illuminate\Http\Request;
 use App\Thread;
 use App\Reply;
+use App\User;
 use App\Rules\SpamFree;
 
 class RepliesController extends Controller
@@ -37,10 +39,24 @@ class RepliesController extends Controller
      */
     public function store(Request $request, $channelId, Thread $thread, CreatePostRequest $createPostRequest)
     {
-        return $thread->addReply([
+        $reply = $thread->addReply([
             'body' => $request->body,
             'user_id' => auth()->id(),
         ])->load('owner');
+
+        preg_match_all('/\@([^\s\.]+)/', $reply->body, $matches);
+
+        $names = $matches[1];
+
+        foreach ($names as $name) {
+            $user = User::whereName($name)->first();
+
+            if ($user) {
+                $user->notify(new YouWereMentioned($reply));
+            }
+        }
+
+        return $reply;
     }
 
     /**
