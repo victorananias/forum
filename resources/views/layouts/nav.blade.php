ca6c0f919701878b9a763064991ec3f9cd92c447
victor <victor@ananias.dev>
2019-04-19 19:37:40 -0300

    save

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -34,8 +34,8 @@
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     @foreach ($channels as $channel)
-                        <a class="dropdown-item" href="/threads/{{ $channel->name }}">
-                            {{ $channel->slug }}
+                        <a class="dropdown-item" href="/threads/{{ $channel->slug }}">
+                            {{ $channel->name }}
                         </a>
                     @endforeach
                 </div>
